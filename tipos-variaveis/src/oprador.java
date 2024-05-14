5c6b4a657399f18e596b2dc8440464831ed35666
Mateus Araújo Monteiro <129124225+MMatteuss@users.noreply.github.com>
2024-05-14 16:07:55 -0300

    operador

---
+++ tipos-variaveis/src/oprador.java
@@ -0,0 +1,7 @@
+public class oprador {
+    public static void main(String[] args) {
+        String nomeComepleto = "LINGUAGEM"+"JAVA";
+        System.err.println(nomeComepleto);
+
+    }
+}
--- tipos-variaveis/src/tiposVariaveis.java
+++ tipos-variaveis/src/tiposVariaveis.java
@@ -1,5 +1,16 @@
 public class tiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //<Tipo> <nome> <atribuição>
+        byte idade = 123;
+        short ano = 2024;
+        int cep = 21070333;
+        long cpf = 987549321L;
+        float pi = 3.14f;
+        double salario = 1234.33;
+        
+        final int ValorNaoPodeMudar = 1;
+      //'final' serve para colocar valor que não poderar se mudado
+
+        System.err.println(ValorNaoPodeMudar);
     }
 }
